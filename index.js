f9d0bb6a16df775088faf9088f0d779124b3d3a2
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-11 08:51:57 +0600

    environ variables added for password safety

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const verifyToken = (req, res, next) => {
     });
 }
 
-const uri = "mongodb+srv://good-admin:6ol0AmyUdoyUyLm0@cluster0.7i1ds.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7i1ds.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
